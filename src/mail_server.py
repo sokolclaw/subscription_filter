d2dd0586d29a335986b80caa2bd4900d179a31ce
sokolclaw <kristoferiork@yandex.ru>
2023-06-19 16:48:46 +0500

    Добавлен класс Settings, куда внесены данные о выборе почтового сервиса. Добавлено логгирование. Добавлено еще одно исключение > ".DS_Store" в .gitignore

--- src/mail_server.py
+++ src/mail_server.py
@@ -1,41 +1,42 @@
 import imaplib
+import logging
+
+from settings import config
+
+logging.basicConfig(filename=config.filename, level=logging.INFO)
 
 class MailServer():
-
+    
     def __init__(self):
 
-        self.mail_servers = {
-            'Mail.ru' : 'imap.mail.ru',
-            'Яндекс.Почта': 'imap.yandex.ru',
-            'Outlook': 'outlook.office365.com',
-            'Exchange': 'mail.contoso.com',
-            'Gmail': 'imap.gmail.com',
-            'iCloud': 'imap.mail.me.com'
-        }
-
-        self.name = input('name: ')
-        self.mail_password = input('password: ')
+        self.servers = config
+        self.name = input('Логин: ')
+        self.mail_password = input('Пароль: ')
 
     def _choose_server(self):
         try:
-            self.mail = input('Which mail? ')
-            return self.mail_servers[self.mail]
+            self.mail = input('Какая почта?  ')
+            return self.servers.mail_servers[self.mail]
         except (KeyError):
+            logging.exception('Неправильный ввод почтового сервиса')
             print('Ошибка. Повторите ввод почтового сервиса')
 
     def autorize(self):
         self.imap = imaplib.IMAP4_SSL(self._choose_server())
         self.imap.login(self.name, self.mail_password)
+        print(self.imap.select('INBOX'))
 
 if __name__ == '__main__':
     ms = MailServer()
     try:
         ms.autorize()
     except ValueError:
+        logging.exception('Неверный логин или пароль')
         print('Неверный логин или пароль')
     except NameError:
-        print('Неправильное имя сервера')
-    except (imaplib.IMAP4.error, ConnectionRefusedError):
-        print('Нет доступа к почте. ' + 
-        'Убедитесь, что даны разрешения на подключение сервиса к почтовому ящику, ' +
-        'а также проверьте корректность логина и пароля')
+        logging.exception('Неправильное имя сервера')
+        print('Неправильный ввод названия почтового сервиса')
+    except (imaplib.IMAP4.error, ConnectionRefusedError, TypeError):
+        logging.exception('Нет доступа к почте')
+        print('''Нет доступа к почте. Убедитесь, что даны разрешения на подключение
+        сервиса к почтовому ящику,а также проверьте корректность логина и пароля''')
---
+++ src/settings.py
@@ -0,0 +1,17 @@
+
+class Settings():
+    
+    def __init__(self):
+
+        self.mail_servers = {
+            'Mail.ru' : 'imap.mail.ru',
+            'Яндекс.Почта': 'imap.yandex.ru',
+            'Outlook': 'outlook.office365.com',
+            'Exchange': 'mail.contoso.com',
+            'Gmail': 'imap.gmail.com',
+            'iCloud': 'imap.mail.me.com'
+        }
+
+        self.filename = 'mail.log'
+
+config = Settings()
